92a0acb60e2f98156443e08fd33cb7f3421a9e9c
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2025-05-04 23:31:51 +0700

    Update Task.js

--- model/Task.js
+++ model/Task.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  description: { type: String, default: '' },
   completed: { type: Boolean, default: false }
 });
 
