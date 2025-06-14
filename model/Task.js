8f5a3f76fa03e46a2504c5e77cbc556b68db1d7f
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2025-06-14 22:39:20 +0700

    Add date

--- model/Task.js
+++ model/Task.js
@@ -1,9 +1,11 @@
+// model/Task.js
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, default: '' },
-  completed: { type: Boolean, default: false }
+  completed: { type: Boolean, default: false },
+  date: { type: Date, default: Date.now } // tambahkan ini
 });
 
 module.exports = mongoose.model('Task', taskSchema);
